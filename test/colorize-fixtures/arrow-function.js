028e0518e3fac944938f11830d81d0de13105412
Ersel Aker <erselaker@gmail.com>
2019-08-03 20:56:11 +0100

    did it!

--- test/colorize-fixtures/arrow-function.js
+++ test/colorize-fixtures/arrow-function.js
@@ -1,3 +1,16 @@
-const arrowFun = (...args) => css`
-  height: 12px;
-`;
+sql`
+select * from table
+select * from table;
+`
+test.mextend`
+  select * from table;
+  insert into
+  color: red;
+  select * from tabl
+`
+
+test.extend`
+color: red;
+`
+
+submitQuery`select * from test;`
